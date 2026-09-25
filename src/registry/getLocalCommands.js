45fd02689bca530288477d3eba1c58260d38a9c3
leo-ishere <110486918+leo-ishere@users.noreply.github.com>
2026-09-25 12:32:29 +0100

    Fix local command loading

--- src/registry/getLocalCommands.js
+++ src/registry/getLocalCommands.js
@@ -1,32 +1,22 @@
 /**
  * getLocalCommands, scans category subfolders for command files
  * @param {string} commandsBasePath - Root commands directory
- * @param {string[]} [exceptions=[]] - Command names to skip (for overriding built-ins)
  * @returns {Object[]} Command objects ready for registration
  */
 
-
-const path = require('path');
 const getAllFiles = require('../utils/getAllFiles');
 
-module.exports = (commandsBasePath, exceptions = []) => {
+module.exports = commandsBasePath => {
   if (!commandsBasePath) return [];
 
-  let localCommands = [];
-
+  const localCommands = [];
   const commandCategories = getAllFiles(commandsBasePath, true);
 
   for (const commandCategory of commandCategories) {
     const commandFiles = getAllFiles(commandCategory);
 
     for (const commandFile of commandFiles) {
-      const commandObject = require(commandFile);
-
-      if (exceptions.includes(commandObject.name)) {
-        continue;
-      }
-
-      localCommands.push(commandObject);
+      localCommands.push(require(commandFile));
     }
   }
 
